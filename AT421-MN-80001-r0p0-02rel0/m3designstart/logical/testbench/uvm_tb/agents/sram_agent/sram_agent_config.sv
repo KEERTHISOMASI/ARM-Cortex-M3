7fe61f70e85f00bbe032f47cbb5635315accead6
KEERTHISOMASI <keerthisree2004@gmail.com>
2025-12-09 11:15:28 +0530

    sram to env

--- AT421-MN-80001-r0p0-02rel0/m3designstart/logical/testbench/uvm_tb/agents/sram_agent/sram_agent_config.sv
+++ AT421-MN-80001-r0p0-02rel0/m3designstart/logical/testbench/uvm_tb/agents/sram_agent/sram_agent_config.sv
@@ -5,11 +5,12 @@ class sram_agent_config extends uvm_object;
   `uvm_object_utils(sram_agent_config)
 
   virtual sram_if vif;
-  sram_storage_c  mem_model; // Shared memory handle
-  bit active = 1;            // Active = Driver + Monitor, Passive = Monitor only
+  sram_storage_c mem_model;  // Shared memory handle
+  bit active = 1;  // Active = Driver + Monitor, Passive = Monitor only
 
   function new(string name = "sram_agent_config");
     super.new(name);
+    bit active = 1;
     mem_model = sram_storage_c::type_id::create("mem_model");
   endfunction
 endclass
