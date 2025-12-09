c17a7a0c55404fb1f90efe715ba29679fd8a46a2
Karthikeya Bandaru <user@localhost.localdomain>
2025-12-09 17:38:46 +0530

    Basic sequence added

---
+++ AT421-MN-80001-r0p0-02rel0/m3designstart/logical/testbench/uvm_tb/tests/iot_ahb_wr_rd_test.sv
@@ -0,0 +1,31 @@
+// Test Intention:
+// 1. To perform write and read any one particular AHB Slave.
+// 2. Verify whether the same written data is being read immediately.
+
+`ifndef IOT_AHB_WR_RD_TEST
+`define IOT_AHB_WR_RD_TEST
+//Include the sequence required
+`include "../sequences/ahb_sequences.sv"
+class iot_ahb_wr_rd_test extends iot_test_base;
+    `uvm_component_utils(iot_ahb_wr_rd_test)
+
+    function new(string name = "iot_test_base", uvm_component parent);
+        super.new(name, parent);
+    endfunction
+
+    function void build_phase(uvm_phase phase);
+        super.build_phase(phase);
+    endfunction
+
+    task run_phase(uvm_phase phase);
+        ahb_write_read_seq seq;
+        phase.raise_objection(this);
+
+        seq = ahb_write_read_seq::type_id::create("seq");
+        seq.start(env.master_agent_dma.sequencer);
+        #100;
+
+        phase.drop_objection(this);
+    endtask
+endclass
+`endif
--- AT421-MN-80001-r0p0-02rel0/m3designstart/logical/testbench/uvm_tb/tests/iot_test_base.sv
+++ AT421-MN-80001-r0p0-02rel0/m3designstart/logical/testbench/uvm_tb/tests/iot_test_base.sv
@@ -19,6 +19,23 @@ class iot_test_base extends uvm_test;
         super.build_phase(phase);
         env = iot_env::type_id::create("env", this);
     endfunction
+    
+    function void end_of_elaboration_phase(uvm_phase phase);
+        uvm_top.print_topology();
 
+        //Configure Slave ranges
+        if (env.slave_agent.driver != null) begin
+        `uvm_info("TEST", "Configuring Slave Address Ranges", UVM_LOW)
+        env.slave_agent_exp0.driver.add_range(32'hA000_0000, 32'hA000_FFFF);
+        env.slave_agent_exp1.driver.add_range(32'h0004_0000, 32'h1FFF_FFFF);
+        env.slave_agent_exp1.driver.add_range(32'h2002_0000, 32'h3FFF_FFFF);
+        env.slave_agent_exp1.driver.add_range(32'h4001_0000, 32'h9FFF_FFFF);
+        env.slave_agent_exp1.driver.add_range(32'hA001_0000, 32'hDFFF_FFFF);
+        env.slave_agent_exp1.driver.add_range(32'hE001_0000, 32'hFFFF_FFFF);
+        end
+        else begin
+        `uvm_error("TEST", "Slave Driver handle is null!")
+        end
+    endfunction
 endclass
 `endif
